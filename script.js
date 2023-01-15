b171443d9f89ddae11b0a61838605aeb08732f19
MaverickEdwards <r.edwards0917@gmail.com>
2023-01-14 22:10:12 -0500

    Update dbJSON

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Testing JS";
+h2.textContent = "My Completed Games List";
 document.querySelector("body").appendChild(h2);
+
+
+document.addEventListener('DOMContentLoaded', fetchGames)
+
+function fetchGames(){
+    fetch('')
+    .then(resp => resp.json())
+    .then(gamesData => renderedGameCard(gamesData))
+}
